809dc9a73672efeab5b9d9a261fdf2375410c1fb
Bruno Laureano <brunolsantos152@gmail.com>
2023-08-02 15:20:27 -0300

    feat: new form to return verifiedcontributor

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -56,6 +56,9 @@ export class UsersService {
       where: {
         address: data.address,
       },
+      include: {
+        VerifiedContributorSubmission: true,
+      },
     });
 
     if (!userExists) {
