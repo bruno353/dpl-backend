b663ba18a57233fd268471bb1ca4c5b1c00da150
Bruno Laureano <brunolsantos152@gmail.com>
2023-08-08 11:37:34 -0300

    feat: increasing budget

--- src/eventsHandler/events-handler.service.ts
+++ src/eventsHandler/events-handler.service.ts
@@ -37,342 +37,6 @@ export class EventsHandlerService {
     private readonly tasksService: TasksService,
     private readonly usersService: UsersService,
   ) {
-    // //event TaskCreated(uint256 taskId, string metadata, uint64 deadline, ERC20Transfer[] budget, address manager, PreapprovedApplication[] preapproved);
-    // this.newcontract.on(
-    //   'TaskCreated',
-    //   async (taskId, proposer, metadata, deadline, budget, event) => {
-    //     const finalData = {
-    //       taskId: String(taskId),
-    //       proposer: proposer,
-    //       metadata: metadata,
-    //       deadline: String(deadline),
-    //       budget: budget,
-    //       blockNumber: String(event.blockNumber),
-    //       blockHash: String(event.blockHash),
-    //       timestamp: String(Date.now()),
-    //       event: event.event,
-    //       transactionHash: event.transactionHash,
-    //       contractAddress: event.address,
-    //     };
-    //     console.log(finalData);
-    //     await this.prisma.event.create({
-    //       data: {
-    //         name: 'TaskCreated',
-    //         data: JSON.stringify(finalData),
-    //       },
-    //     });
-    //     await this.tasksService.updateSingleTaskData(taskId);
-    //     this.tasksService.updateTasksData();
-    //   },
-    // );
-    // //event ApplicationCreated(uint256 taskId, uint16 applicationId, string metadata, Reward[] reward, address proposer, address applicant);
-    // this.newcontract.on(
-    //   'ApplicationCreated',
-    //   async (
-    //     taskId,
-    //     applicationId,
-    //     metadata,
-    //     reward,
-    //     proposer,
-    //     msgSender,
-    //     event,
-    //   ) => {
-    //     const finalData = {
-    //       taskId: String(taskId),
-    //       applicationId: String(applicationId),
-    //       proposer: proposer,
-    //       msgSender: msgSender,
-    //       metadata: metadata,
-    //       reward: JSON.stringify(reward),
-    //       blockNumber: String(event.blockNumber),
-    //       blockHash: String(event.blockHash),
-    //       timestamp: String(Date.now()),
-    //       event: event.event,
-    //       transactionHash: event.transactionHash,
-    //       contractAddress: event.address,
-    //     };
-    //     console.log(finalData);
-    //     await this.prisma.event.create({
-    //       data: {
-    //         name: 'ApplicationCreated',
-    //         data: JSON.stringify(finalData),
-    //       },
-    //     });
-    //     await this.tasksService.updateSingleTaskData(taskId);
-    //     this.tasksService.updateTasksData();
-    //   },
-    // );
-    // //event ApplicationAccepted(uint256 taskId, uint16 application, address proposer, address applicant);
-    // this.newcontract.on(
-    //   'ApplicationAccepted',
-    //   async (taskId, applicationId, proposer, applicant, event) => {
-    //     const finalData = {
-    //       taskId: String(taskId),
-    //       applicationId: String(applicationId),
-    //       proposer: proposer,
-    //       applicant: applicant,
-    //       blockNumber: String(event.blockNumber),
-    //       blockHash: String(event.blockHash),
-    //       timestamp: String(Date.now()),
-    //       event: event.event,
-    //       transactionHash: event.transactionHash,
-    //       contractAddress: event.address,
-    //     };
-    //     console.log(finalData);
-    //     await this.prisma.event.create({
-    //       data: {
-    //         name: 'ApplicationAccepted',
-    //         data: JSON.stringify(finalData),
-    //       },
-    //     });
-    //     await this.tasksService.updateSingleTaskData(taskId);
-    //     this.tasksService.updateTasksData();
-    //   },
-    // );
-    // //event SubmissionCreated(uint256 taskId, uint8 submissionId, string metadata, address proposer, address executor);
-    // this.newcontract.on(
-    //   'SubmissionCreated',
-    //   async (taskId, submissionId, metadata, proposer, executor, event) => {
-    //     const finalData = {
-    //       taskId: String(taskId),
-    //       submissionId: String(submissionId),
-    //       metadata: metadata,
-    //       proposer: proposer,
-    //       executor: executor,
-    //       blockNumber: String(event.blockNumber),
-    //       blockHash: String(event.blockHash),
-    //       timestamp: String(Date.now()),
-    //       event: event.event,
-    //       transactionHash: event.transactionHash,
-    //       contractAddress: event.address,
-    //     };
-    //     console.log(finalData);
-    //     await this.prisma.event.create({
-    //       data: {
-    //         name: 'SubmissionCreated',
-    //         data: JSON.stringify(finalData),
-    //       },
-    //     });
-    //     await this.tasksService.updateSingleTaskData(taskId);
-    //     this.tasksService.updateTasksData();
-    //   },
-    // );
-    // //event SubmissionReviewed(uint256 taskId, uint8 submissionId, SubmissionJudgement judgement, string feedback, address proposer, address executor);
-    // this.newcontract.on(
-    //   'SubmissionReviewed',
-    //   async (
-    //     taskId,
-    //     submissionId,
-    //     judgement,
-    //     feedback,
-    //     proposer,
-    //     executor,
-    //     event,
-    //   ) => {
-    //     const finalData = {
-    //       taskId: String(taskId),
-    //       submissionId: String(submissionId),
-    //       judgement: JSON.stringify(judgement),
-    //       feedback: JSON.stringify(feedback),
-    //       proposer: proposer,
-    //       executor: executor,
-    //       blockNumber: String(event.blockNumber),
-    //       blockHash: String(event.blockHash),
-    //       timestamp: String(Date.now()),
-    //       event: event.event,
-    //       transactionHash: event.transactionHash,
-    //       contractAddress: event.address,
-    //     };
-    //     console.log(finalData);
-    //     await this.prisma.event.create({
-    //       data: {
-    //         name: 'SubmissionReviewed',
-    //         data: JSON.stringify(finalData),
-    //       },
-    //     });
-    //     await this.tasksService.updateSingleTaskData(taskId);
-    //     this.tasksService.updateTasksData();
-    //   },
-    // );
-    // //event TaskCompleted(uint256 taskId, address proposer, address executor);
-    // this.newcontract.on(
-    //   'TaskCompleted',
-    //   async (taskId, proposer, executor, event) => {
-    //     const finalData = {
-    //       taskId: String(taskId),
-    //       proposer: proposer,
-    //       executor: executor,
-    //       blockNumber: String(event.blockNumber),
-    //       blockHash: String(event.blockHash),
-    //       timestamp: String(Date.now()),
-    //       event: event.event,
-    //       transactionHash: event.transactionHash,
-    //       contractAddress: event.address,
-    //     };
-    //     console.log(finalData);
-    //     await this.prisma.event.create({
-    //       data: {
-    //         name: 'TaskCompleted',
-    //         data: JSON.stringify(finalData),
-    //       },
-    //     });
-    //     await this.tasksService.updateSingleTaskData(taskId);
-    //     this.tasksService.updateTasksData();
-    //   },
-    // );
-    // //event ChangeScopeRequested(uint256 taskId, uint8 requestId, string metadata, uint64 deadline, Reward[] reward);
-    // this.newcontract.on(
-    //   'ChangeScopeRequested',
-    //   async (taskId, proposer, executor, event) => {
-    //     const finalData = {
-    //       taskId: String(taskId),
-    //       proposer: proposer,
-    //       executor: executor,
-    //       blockNumber: String(event.blockNumber),
-    //       blockHash: String(event.blockHash),
-    //       timestamp: String(Date.now()),
-    //       event: event.event,
-    //       transactionHash: event.transactionHash,
-    //       contractAddress: event.address,
-    //     };
-    //     console.log(finalData);
-    //     await this.prisma.event.create({
-    //       data: {
-    //         name: 'ChangeScopeRequested',
-    //         data: JSON.stringify(finalData),
-    //       },
-    //     });
-    //     await this.tasksService.updateSingleTaskData(taskId);
-    //     this.tasksService.updateTasksData();
-    //   },
-    // );
-    // //event DropExecutorRequested(uint256 taskId, uint8 requestId, string explanation);
-    // this.newcontract.on(
-    //   'DropExecutorRequested',
-    //   async (taskId, requestId, explanation, event) => {
-    //     const finalData = {
-    //       taskId: String(taskId),
-    //       requestId: String(requestId),
-    //       explanation: explanation,
-    //       blockNumber: String(event.blockNumber),
-    //       blockHash: String(event.blockHash),
-    //       timestamp: String(Date.now()),
-    //       event: event.event,
-    //       transactionHash: event.transactionHash,
-    //       contractAddress: event.address,
-    //     };
-    //     console.log(finalData);
-    //     await this.prisma.event.create({
-    //       data: {
-    //         name: 'newcontract',
-    //         data: JSON.stringify(finalData),
-    //       },
-    //     });
-    //     await this.tasksService.updateSingleTaskData(taskId);
-    //     this.tasksService.updateTasksData();
-    //   },
-    // );
-    // //event CancelTaskRequested(uint256 taskId, uint8 requestId, string explanation);
-    // this.newcontract.on(
-    //   'CancelTaskRequested',
-    //   async (taskId, requestId, explanation, event) => {
-    //     const finalData = {
-    //       taskId: String(taskId),
-    //       requestId: String(requestId),
-    //       explanation: explanation,
-    //       blockNumber: String(event.blockNumber),
-    //       blockHash: String(event.blockHash),
-    //       timestamp: String(Date.now()),
-    //       event: event.event,
-    //       transactionHash: event.transactionHash,
-    //       contractAddress: event.address,
-    //     };
-    //     console.log(finalData);
-    //     await this.prisma.event.create({
-    //       data: {
-    //         name: 'CancelTaskRequested',
-    //         data: JSON.stringify(finalData),
-    //       },
-    //     });
-    //     await this.tasksService.updateSingleTaskData(taskId);
-    //     this.tasksService.updateTasksData();
-    //   },
-    // );
-    // //event RequestAccepted(uint256 taskId, RequestType requestType, uint8 requestId);
-    // this.newcontract.on(
-    //   'RequestAccepted',
-    //   async (taskId, requestType, requestId, event) => {
-    //     const finalData = {
-    //       taskId: String(taskId),
-    //       requestId: String(requestId),
-    //       requestType: JSON.stringify(requestType),
-    //       blockNumber: String(event.blockNumber),
-    //       blockHash: String(event.blockHash),
-    //       timestamp: String(Date.now()),
-    //       event: event.event,
-    //       transactionHash: event.transactionHash,
-    //       contractAddress: event.address,
-    //     };
-    //     console.log(finalData);
-    //     await this.prisma.event.create({
-    //       data: {
-    //         name: 'RequestAccepted',
-    //         data: JSON.stringify(finalData),
-    //       },
-    //     });
-    //     await this.tasksService.updateSingleTaskData(taskId);
-    //     this.tasksService.updateTasksData();
-    //   },
-    // );
-    // //event RequestExecuted(uint256 taskId, RequestType requestType, uint8 requestId, address by);
-    // this.newcontract.on(
-    //   'RequestExecuted',
-    //   async (taskId, requestType, requestId, by, event) => {
-    //     const finalData = {
-    //       taskId: String(taskId),
-    //       requestId: String(requestId),
-    //       requestType: JSON.stringify(requestType),
-    //       by: by,
-    //       blockNumber: String(event.blockNumber),
-    //       blockHash: String(event.blockHash),
-    //       timestamp: String(Date.now()),
-    //       event: event.event,
-    //       transactionHash: event.transactionHash,
-    //       contractAddress: event.address,
-    //     };
-    //     console.log(finalData);
-    //     await this.prisma.event.create({
-    //       data: {
-    //         name: 'RequestExecuted',
-    //         data: JSON.stringify(finalData),
-    //       },
-    //     });
-    //     await this.tasksService.updateSingleTaskData(taskId);
-    //     this.tasksService.updateTasksData();
-    //   },
-    // );
-    // //event TaskCancelled(uint256 taskId);
-    // this.newcontract.on('TaskCancelled', async (taskId, event) => {
-    //   const finalData = {
-    //     taskId: String(taskId),
-    //     blockNumber: String(event.blockNumber),
-    //     blockHash: String(event.blockHash),
-    //     timestamp: String(Date.now()),
-    //     event: event.event,
-    //     transactionHash: event.transactionHash,
-    //     contractAddress: event.address,
-    //   };
-    //   console.log(finalData);
-    //   await this.prisma.event.create({
-    //     data: {
-    //       name: 'TaskCancelled',
-    //       data: JSON.stringify(finalData),
-    //     },
-    //   });
-    //   await this.tasksService.updateSingleTaskData(taskId);
-    //   this.tasksService.updateTasksData();
-    // });
-
     //event ApplicationCreated(uint256 taskId, uint16 applicationId, string metadata, Reward[] reward, address proposer, address applicant);
     this.newcontract.on(
       'ApplicationCreated',
@@ -872,6 +536,84 @@ export class EventsHandlerService {
           },
           data: {
             status: '2',
+          },
+        });
+        this.usersService.checkIfUserExistsOnTheChain(executor);
+      },
+    );
+
+    //event BudgetIncreased(uint256 indexed taskId, uint96[] increase, address manager);
+    this.newcontract.on(
+      'BudgetIncreased',
+      async (taskId, increase, executor, event) => {
+        console.log('new event');
+        console.log(event);
+        console.log('event event');
+        console.log(event.event);
+
+        const block = await this.web3Provider.getBlock(event['blockNumber']);
+        const timestamp = String(block.timestamp) || String(Date.now() / 1000); // Timestamp in seconds
+
+        //storing on the "events" table
+        const finalData = {
+          event: event,
+          contractAddress: event.address,
+        };
+        console.log(finalData);
+        await this.prisma.event.create({
+          data: {
+            name: 'BudgetIncreased',
+            data: JSON.stringify(finalData),
+            eventIndex: String(event.logIndex),
+            transactionHash: event.transactionHash,
+            blockNumber: String(event.blockNumber),
+            taskId: String(taskId),
+            address: executor,
+            timestamp: timestamp,
+          },
+        });
+        //getting all the payments from the task and then checking if its increased something:
+        const payments = await this.prisma.payment.findMany({
+          where: {
+            taskId: String(taskId),
+          },
+        });
+        console.log('os payments');
+        console.log(payments);
+        console.log('os incrementes');
+        console.log(increase);
+        for (let i = 0; i < payments.length; i++) {
+          if (Number(increase[i]) > 0) {
+            console.log('increase');
+            await this.prisma.payment.update({
+              where: {
+                id: payments[i].id,
+              },
+              data: {
+                amount: String(
+                  Number(payments[i].amount) + Number(increase[i]),
+                ),
+              },
+            });
+          }
+        }
+        console.log('final payment');
+        const finalPayments = await this.prisma.payment.findMany({
+          where: {
+            taskId: String(taskId),
+          },
+        });
+        console.log('budgetTask');
+        const budgetTask = await this.tasksService.getEstimateBudgetToken(
+          finalPayments,
+        );
+        console.log('task');
+        this.prisma.task.update({
+          where: {
+            taskId: String(taskId),
+          },
+          data: {
+            estimatedBudget: budgetTask,
           },
         });
         this.usersService.checkIfUserExistsOnTheChain(executor);
