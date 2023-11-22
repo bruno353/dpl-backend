c0620ba74319e2c4310700dd882d0190a4fd606b
Bruno Laureano <brunolsantos152@gmail.com>
2023-11-22 01:17:42 -0300

    feat: new deploy

--- src/xnodes/testing.service.ts
+++ src/xnodes/testing.service.ts
@@ -1,35 +1,37 @@
-import {
-  ConflictException,
-  Injectable,
-  BadRequestException,
-  UnauthorizedException,
-} from '@nestjs/common';
-
-import { exec } from 'child_process';
-import { promisify } from 'util';
-
+import { spawn } from 'child_process';
+import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../database/prisma.service';
 
 @Injectable()
 export class TestingService {
   constructor(private readonly prisma: PrismaService) {}
-  private execPromise = promisify(exec);
 
-  async createWallet(identity: string): Promise<string> {
-    console.log('will start');
-    console.log(`PATH: ${process.env.PATH}`);
-    try {
-      const { stdout, stderr } = await this.execPromise(
-        `dfx identity new ${identity}`,
-        { maxBuffer: 1024 * 1024 * 10 }, // 10 MB
-      );
-      if (stderr) {
-        throw new Error(stderr);
-      }
-      console.log(stdout);
-      return stdout;
-    } catch (error) {
-      throw new Error(`Erro ao criar wallet: ${error.message}`);
-    }
+  createWallet(identity: string): Promise<string> {
+    return new Promise((resolve, reject) => {
+      const output = { stdout: '', stderr: '' };
+      const dfx = spawn('dfx', ['identity', 'new', identity]);
+
+      dfx.stdout.on('data', (data) => {
+        output.stdout += data.toString();
+      });
+
+      dfx.stderr.on('data', (data) => {
+        output.stderr += data.toString();
+      });
+
+      dfx.on('close', (code) => {
+        if (code === 0) {
+          console.log('dfx command executed successfully:', output.stdout);
+          resolve(output.stdout);
+        } else {
+          console.error('dfx command failed:', output.stderr);
+          reject(new Error(`Erro ao criar wallet: ${output.stderr}`));
+        }
+      });
+
+      dfx.on('error', (error) => {
+        reject(new Error(`Erro ao criar wallet: ${error.message}`));
+      });
+    });
   }
 }
